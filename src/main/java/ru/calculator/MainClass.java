f9bf83fe1c7d187e5fe8f06992a67f61d2f89aaa
nikolaibukanov <r1483770@mvrht.net>
2019-07-06 14:22:30 +0300

    close scanner

--- src/main/java/ru/calculator/MainClass.java
+++ src/main/java/ru/calculator/MainClass.java
@@ -9,10 +9,11 @@ import java.util.Scanner;
 //TODO: добавить доку и тесты
 public class MainClass {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Please enter two numbers from 1 to 10 separated by  math symbol such as “+”, “-”, “*”, “/”." +
                 " You can use arabic or roman numbers. Examples: '2 + 2', 'X * II'git ");
-        Scanner scanner = new Scanner(System.in);
         while (true) {
             String userInput = scanner.nextLine();
             Calculator calculator = calculatorSelection(userInput);
@@ -26,6 +27,7 @@ public class MainClass {
         } if (userInput.matches("^\\s*([-+]?)(?:(X|IX|IV|V?I{0,3}))(?:\\s*([-+*\\/])\\s*((?:\\s[-+])?(X|IX|IV|V?I{0,3}))\\s*)+$")){
             return new RomanCalculator();
         } else {
+            scanner.close();
             throw new BaseRuntimeException("Incorrect input. The program was terminated.");
         }
     }
